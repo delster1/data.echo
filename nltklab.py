2309ae4e6b0b731bb98f41a335722f635eddd3fa
delster1 <delsterone@gmail.com>
2022-11-12 15:06:07 -0600

    lemmatized input so that plurals arent annoying

--- nltklab.py
+++ nltklab.py
@@ -1,27 +1,35 @@
 import nltk
 from nltk.corpus import stopwords  # stop words from nltk
 from nltk.tokenize import word_tokenize  # tokenizer function
+from nltk.stem import WordNetLemmatizer
 
 nltk.download("punkt")  #	downloads
 nltk.download("stopwords")  #	downloads
 nltk.download("averaged_perceptron_tagger")  #	downloads
+lemmatizer = WordNetLemmatizer()
 
-stoplist = set(
- stopwords.words("english"))  #	initalized stopwords in english from nltk
+stoplist = stopwords.words("english")  #	initalized stopwords in english from nltk
 
 sentence = input("enter a question\n")  #	sentence to be processed
 
 wordsInSentence = word_tokenize(sentence)  # tokenizing sentence by
-
+wordsInSentence = [lemmatizer.lemmatize(word) for word in wordsInSentence]
 taggedSentence = nltk.pos_tag(wordsInSentence)  # tagging sentence by POS
+print(taggedSentence)
+cswords = open('cswords.txt', 'r')
+cswordslines = cswords.readlines()
+
+toResearch = []
 
 filteredSentence = []
-for index,word in enumerate(wordsInSentence):  # iterate through tokenized sentence
-    if word.casefold() not in stoplist or taggedSentence[index][1][0] != "P":
-        filteredSentence.append(word)  
-        print(taggedSentence[index])  # detect non-filler words
+for line in cswordslines:
+    for word in wordsInSentence:
+        if word in stoplist:
+            wordsInSentence.remove(word)
+        elif word in line:
+            toResearch.append([word,line])
 
 
-print(
- f"\nOriginal Sentence: {sentence}\n\nTokenized Sentence:\n {wordsInSentence}\n\nTagged Sentence: (look at the tagging slide to get this)\n{taggedSentence}\n\nFiltered Sentence:\n {filteredSentence}\n\n"
-)
+# print(cswords)
+
+print("toResearch: ", toResearch)
