43636461f7634e4221be39f4df4cad8154e6501c
Birduo <dkneidel@icloud.com>
2022-11-13 06:03:32 -0600

    updated mainfile

--- dataecho.py
+++ dataecho.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import warnings
 import whisper
 import recordaudio as ra
@@ -8,51 +8,59 @@ from bs4 import BeautifulSoup as bs
 import cswiki
 import nltktools as nql
 
-# Transcribing Audio
-
-# ignore warnings caused by model.transcribe()
-warnings.filterwarnings(action='ignore', category=UserWarning)
-
-ra.record_audio()
-
-model = whisper.load_model('base.en')
-
-result = str(model.transcribe('input.wav')['text'])
-
-os.remove('input.wav')
-
-# args = ['web crawler']
-
-qtype, args = nql.findType(nql.tagWords(result))
-print(qtype, args)
-
-#to create output file
-outfile = open('out.html', 'w')
-
-# (sentence: str, args: list)
-markup = cswiki.search_cswiki(result, args) # search wikipedia w/ result
-
-if markup is not None:
-    match qtype:
-        case "WHAT":
-            cleanHTML = cswiki.clean_markup(markup, True, 'wiki').prettify()
-            
-            myfile = open("whathalfone.txt", "r")
-            outfile.writelines(myfile.readlines())
-            myfile.close()
-
-            outfile.write(cleanHTML)
-
-            myfile = open("whathalftwo.txt", "r")
-            outfile.writelines(myfile.readlines())
-            myfile.close()
-
-            soup = bs(cleanHTML, 'html.parser')
-
-            print(soup.contents)
-
-
+def main():
+    cmd_argc = len(sys.argv)
+    match cmd_argc:
         case _:
-            print('default case')
+            print(cmd_argc)
 
+    # ignore warnings caused by model.transcribe()
+    warnings.filterwarnings(action='ignore', category=UserWarning)
+
+    # record audio
+    ra.record_audio()
+
+    model = whisper.load_model('base.en')
+
+    # transcribe audio
+    sentence = str(model.transcribe('input.wav')['text'])
+
+    print(f'\n----------------\nTranscribed audio: {sentence}')
+
+    os.remove('input.wav')
+
+    qtype, args = nql.findType(nql.tagWords(sentence))
+    print(qtype, args)
+
+    #to create output file
+    outfile = open('out.html', 'w')
+
+    # (sentence: str, args: list)
+    markup = cswiki.search_cswiki(sentence, args) # search wikipedia w/ result
+
+    if markup is not None:
+        match qtype:
+            case "WHAT":
+                cleanHTML = cswiki.clean_markup(markup, True, 'wiki').prettify()
+                
+                myfile = open("whathalfone.txt", "r")
+                outfile.writelines(myfile.readlines())
+                myfile.close()
+
+                outfile.write(cleanHTML)
+
+                myfile = open("whathalftwo.txt", "r")
+                outfile.writelines(myfile.readlines())
+                myfile.close()
+
+                soup = bs(cleanHTML, 'html.parser')
+
+                print(soup.contents)
+
+
+            case _:
+                print('default case')
+
+if __name__ == '__main__':
+    main() 
     
